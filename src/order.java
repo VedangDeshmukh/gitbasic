c01ed48f5de94803f40d1ecd52e4f7230fb52e5e
VedangDeshmukh <deshmukhvedang7@gmail.com>
2022-06-27 18:36:21 +0530

    added order modify logic in order.java

--- src/order.java
+++ src/order.java
@@ -5,4 +5,8 @@ public class order {
 	public void createorder() {
 		System.out.println("create order feature is added");
 	}
+	
+	public void modifyordre() {
+		System.out.println("modify order feature is add");
+	}
 }
