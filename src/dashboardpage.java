488899d32c88699611d69c38507e3dc3dfc2386e
VedangDeshmukh <deshmukhvedang7@gmail.com>
2022-06-27 22:05:21 +0530

    added total count and country method to dashbaordpage.java and added dashbaordutil.java clas

--- src/dashboardpage.java
+++ src/dashboardpage.java
@@ -4,5 +4,12 @@ public class dashboardpage {
 	public void creategrid() {
 		System.out.println("grid is created");
 	}
-
+	
+	public void totalsale() {
+		System.out.println("total sale feature");
+	}
+	
+	public void totalcountry() {
+		System.out.println("total country");
+	}
 }
